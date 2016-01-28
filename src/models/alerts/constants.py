7fdccb41b27b3fbb3b2ba9bbe9577fd5bd8f7604
jslvtr <jslvtr@gmail.com>
2016-01-28 16:12:32 +0000

    Updating constants

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,8 @@
 __author__ = 'jslvtr'
 
 
-URL = "https://api.mailgun.net/v3/sandbox0fd1d065f521484b8af277034648e756.mailgun.org/messages"
-API_KEY = "key-4dbf00d53920bd7325342ce457057317"
-FROM = "Mailgun Sandbox <postmaster@sandbox0fd1d065f521484b8af277034648e756.mailgun.org>"
+URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
+API_KEY = "key-<>"
+FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
