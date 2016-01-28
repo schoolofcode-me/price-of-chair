11329050647b48c37c3080d8de4453d4f814287e
jslvtr <jslvtr@gmail.com>
2016-01-28 14:20:51 +0000

    Adding store index template and view to create store (although with no content yet).

--- src/models/stores/views.py
+++ src/models/stores/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 __author__ = 'jslvtr'
 
@@ -8,9 +8,14 @@ store_blueprint = Blueprint('stores', __name__)
 
 @store_blueprint.route('/')
 def index():
-    return "This is the Store Index"
+    return render_template('stores/store_index.jinja2')
 
 
-@store_blueprint.route('/store/<string:name>')
+@store_blueprint.route('/new', methods=['GET', 'POST'])
+def create_store():
+    return "This is the create store page."
+
+
+@store_blueprint.route('/<string:name>')
 def store_page():
     pass
